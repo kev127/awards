fc812979d5d0b20e80c1c17f70384ce62ad9d599
kev127 <kelvinkeya125@gmail.com>
2020-11-30 13:21:00 +0300

    update code

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .forms import UserUpdateForm, ProfileUpdateForm, SignUpForm, NewProjectForm, VoteForm
 from .models import Profile, Project, Comment
 from django.contrib.auth.decorators import login_required
- from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login
 
 def welcome(request):
     return render(request, 'welcome.html')
